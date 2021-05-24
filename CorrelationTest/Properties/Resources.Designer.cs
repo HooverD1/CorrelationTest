3d84b7623edc4dab6560d4ab7ec6acfd6dd42962
David_Hoover <davehoover1985@gmail.com>
2021-05-24 15:37:09 -0500

    Made the cursor transparent for the correlation paint can tool.

--- CorrelationTest/Properties/Resources.Designer.cs
+++ CorrelationTest/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace CorrelationTest.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap CircleCursor2 {
+            get {
+                object obj = ResourceManager.GetObject("CircleCursor2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
